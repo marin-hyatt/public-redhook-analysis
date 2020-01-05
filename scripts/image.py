b708998d290e7811526b0fdfc44fd3cfdace716e
marin-hyatt <marin.hyatt@gmail.com>
2020-01-04 22:12:17 -0500

    Fixed problem where same truck appeared multiple times

--- scripts/image.py
+++ scripts/image.py
@@ -1,10 +1,15 @@
+import sys
 import cv2
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 
-df = pd.read_csv('max_dataframe.csv')
+print("done importing")
+sys.stdout.flush()
+
+df = pd.read_csv('/green-projects/project-sonyc_redhook/workspace/share/redhook-analysis/output/max_dataframe.csv')
+print("read csv")
+sys.stdout.flush()
 
 def show_frame(video_file, frame_number):
     vid_obj = cv2.VideoCapture(video_file)
@@ -15,10 +20,19 @@ def show_frame(video_file, frame_number):
         return cropped_frame
     else:
         return False
+    
+print("defined function")
+sys.stdout.flush()
 
 img_arr = [show_frame('/green-projects/project-sonyc_redhook/workspace/share/original_video/' \
                + str(int(df.iloc[x]['start_timestamp'])) + '.ts', df.iloc[x]['frame']) for x in df.index]
 
+print("calculated img_arr")
+sys.stdout.flush()
+
 df['image'] = img_arr
 
-df.to_hdf('max_dataframe_img.hdf5')
+df.to_hdf('max_dataframe_img.hdf5', key='df')
+
+print("saved to hdf5")
+sys.stdout.flush()
